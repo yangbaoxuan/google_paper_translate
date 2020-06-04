ce6020c9d83d6ccc9ed2f148635eae10938c9a9d
yangbaoxuan <784504195@qq.com>
2020-06-04 12:47:44 +0800

    Update google_t.py

--- google_t.py
+++ google_t.py
@@ -1,27 +1,26 @@
-from googletrans import Translator
 from selenium import webdriver
-import socks
-import socket
 import os
 import time
 
-browser = webdriver.Chrome('D:\Program Files (x86)\chromedriver\chromedriver.exe')
 
 def zh(text):
     url='https://translate.google.cn/#view=home&op=translate&sl=auto&tl=zh-CN&text='+text
     browser.get(url)
-    time.sleep(5)
+    time.sleep(2)   #等待页面刷新
     jg=browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div[3]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/div/span[1]')
     return jg.text
 
 
-socks.set_default_proxy(proxy_type=socks.SOCKS5, addr="127.0.0.1", port=10808, rdns=True, )
-socket.socket = socks.socksocket
-outfile='temp.txt'
-imputfile="bare_conf.pdf"
+outfile='temp.txt'  #输出结果txt
+imputfile="bare_conf.pdf"  #pdf文件目录
+
+browser = webdriver.Chrome('D:\Program Files (x86)\chromedriver\chromedriver.exe')
+
 os.system("pdf2txt.py  -o "+outfile+" "+imputfile)
 f = open(outfile,"r",encoding='utf-8')   #设置文件对象
 str = f.read()     #将txt文件的所有内容读入到字符串str中
+f.close()
+
 str=str.replace('\n\n','000000')
 str=str.replace('-\n','')
 str=str.replace('\n',' ')
@@ -41,6 +40,6 @@ for i in strs:
 src_text=stt
 t=zh(src_text)
 pp+=t
-f2=open("tt.txt","w+",encoding='utf-8')
+f2=open(outfile,"w+",encoding='utf-8')
 f2.write(pp)
 f2.close()
